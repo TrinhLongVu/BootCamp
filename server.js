71278158b409359fc7d6b6cd6719fdf13913ab3b
Long Vu <vucongleo@gmail.com>
2024-05-18 12:33:22 +0700

    Fix: port running server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./src/app')
-const config = require('./src/configs/mongodb.config')
-const PORT = config.app.port || 3000;
+const PORT = 3000;
 
 const server = app.listen(PORT, () => {
     console.log(`start project....${PORT}`)
